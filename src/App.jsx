3260daf1fb02bd8d1bd9590f56ed62505e875d84
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-08-26 02:29:41 -0400

    adding routes for different components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./components/Hero/Hero.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
