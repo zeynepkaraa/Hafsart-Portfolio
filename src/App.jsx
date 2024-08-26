852b78e3f71dc1e6265abc89d5bc78efb5d068f3
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-08-26 03:42:01 -0400

    adding Home component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
+
 import Hero from "./components/Hero/Hero.jsx";
+import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
+      <Home />
     </div>
   );
 }
