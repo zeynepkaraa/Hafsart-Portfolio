a80619227861120fa00db66d225b418f7f09ec7a
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-08-27 02:08:25 -0400

    updating card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { FaHeart } from "react-icons/fa"; // Import the icon you want to use for all cards
 import styles from "./Card.module.css"; // Assuming you're using CSS modules
+import { RiDoubleQuotesL } from "react-icons/ri";
 
-const Card = ({ title, icon, description }) => {
+const Card = ({ review, clientImg, clientName, eventType }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.icon}>{icon}</div>
-      <h3 className={styles.title}>{title}</h3>
-      <p className={styles.description}>{description}</p>
+      <div className={styles.icon}>
+        <RiDoubleQuotesL />
+      </div>
+      <p className={styles.review}>{review}</p>
+      <div className={styles.clientInfo}>
+        <img src={clientImg} alt={clientName} className={styles.clientImg} />
+        <div>
+          <h4 className={styles.clientName}>{clientName}</h4>
+          <p className={styles.eventType}>{eventType}</p>
+        </div>
+      </div>
     </div>
   );
 };
