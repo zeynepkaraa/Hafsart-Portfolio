bd11e82d3375d672f8247aef9988e95ecb72bad2
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-08-27 02:43:55 -0400

    state change to navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
 import styles from "./Navbar.module.css";
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+
+  // Check if the current path is the home page
+  const isHomePage = location.pathname === "/";
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
+      <a
+        className={`${styles.title} ${!isHomePage ? styles.newTitle : ""}`}
+        href="/"
+      >
         Hafsart
       </a>
 
@@ -20,14 +28,14 @@ const Navbar = () => {
           onClick={() => setMenuOpen(false)}
         >
           <li>
-            <a href="#portfolio">Portfolio</a>
+            <a href="/portfolio">Portfolio</a>
           </li>
 
           <li>
-            <a href="#about">About</a>
+            <a href="/about">About</a>
           </li>
           <li>
-            <a href="#book">Book</a>
+            <a href="/book">Book</a>
           </li>
         </ul>
       </div>
