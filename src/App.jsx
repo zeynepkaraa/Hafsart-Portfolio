3740cd3ab5e4ef5beee2c83e5954b518263cf585
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-08-27 02:43:02 -0400

    updates routes accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import styles from "./App.module.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Hero from "./components/Hero/Hero.jsx";
 import HomePage from "./components/Pages/HomePage/Home.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-
+import AboutPage from "./components/Pages/AboutPage/About.jsx";
 function App() {
   return (
-    <div className={styles.App}>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Public Routes */}
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
