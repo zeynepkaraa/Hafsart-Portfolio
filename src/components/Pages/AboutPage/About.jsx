47e3362e82d309807a703ffe043ab72fe007fcda
zeynepkaraa <zeynepkara.tmu@gmail.com>
2024-09-01 01:11:59 -0400

    removed svg sticker

--- src/components/Pages/AboutPage/About.jsx
+++ src/components/Pages/AboutPage/About.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../../Navbar/Navbar";
 import Footer from "../../Footer/Footer";
 import styles from "./About.module.css";
-import Flower from "../../../../assets/images/flower.svg";
 const About = () => {
   return (
     <div className={styles.page}>
